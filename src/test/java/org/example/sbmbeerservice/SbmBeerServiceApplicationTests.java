f79b7e100eccc8667236d3821d2950b64d6ba727
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-30 11:36:07 +0100

    initial commit

---
+++ src/test/java/org/example/sbmbeerservice/SbmBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbmbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
