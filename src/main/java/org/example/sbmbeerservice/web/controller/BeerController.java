307704feaca6da76c5be56e4b596a8560b0f70d9
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-30 13:42:23 +0100

    Setup for CRUD operations in controller.

---
+++ src/main/java/org/example/sbmbeerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package org.example.sbmbeerservice.web.controller;
+
+import org.example.sbmbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> findBeerById(@PathVariable("beerId") String beerId) {
+
+        //todo implementation get beer
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createBeer(@RequestBody BeerDto beerDto) {
+        //todo implementation post beer
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<?> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //todo implementation put/update beer
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+}
