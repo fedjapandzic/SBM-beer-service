f3239b33194fca79d01c56f8214afd699e75b755
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-31 11:55:39 +0100

    Adding beer repository

---
+++ src/main/java/org/example/sbmbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package org.example.sbmbeerservice.repositories;
+
+import org.example.sbmbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
