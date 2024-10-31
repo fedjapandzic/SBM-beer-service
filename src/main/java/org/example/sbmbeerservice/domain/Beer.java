5d088cbb4aafa3dddb58b719bac1bbbac4c390c0
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-31 11:52:51 +0100

    Adding beer JPA entity

---
+++ src/main/java/org/example/sbmbeerservice/domain/Beer.java
@@ -0,0 +1,43 @@
+package org.example.sbmbeerservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(length = 36, nullable = false,updatable = false,columnDefinition = "varchar")
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
+
